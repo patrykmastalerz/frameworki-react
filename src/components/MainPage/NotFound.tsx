b1b922c2cdec308926f04586993e7ef25f990c33
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-06-15 21:52:05 +0200

    notfound page

--- src/components/MainPage/NotFound.tsx
+++ src/components/MainPage/NotFound.tsx
@@ -8,14 +8,18 @@ import {fontSize} from '../../styledHelpers/FontSizes';
 
 const Wrapper = styled.section`
   background-color: ${Colors.white};
-  width: 200px;
+  width: 1200px;
+  margin-left: 10px;
+  text-align: center;
+  font-weight: bold;
+  font-size: ${fontSize[24]};
 
 `;
 
 const NotFound: FC = () => {
   return (
     <Wrapper>
-        Notfound
+        Strona nieobsługiwana!
     </Wrapper>
   );
 }
