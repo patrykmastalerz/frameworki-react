42809ca3778d0d50e926e8db1522fae8392e3053
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-06-15 15:36:59 +0200

    poprawiony profil

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
+import React, { FC, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 import {Colors} from '../../styledHelpers/Colors';
 import {fontSize} from '../../styledHelpers/FontSizes';
 
@@ -9,25 +10,28 @@ import {fontSize} from '../../styledHelpers/FontSizes';
 const Wrapper = styled.section`
   background-color: ${Colors.white};
   width: 1000px;
+  margin-left: 10px;
 `;
 
 const TopBar = styled.div`
   width: 100%;
-  background-color: yellow;
+  background-color: white;
   display: flex;
   justify-content: flex-end;
 `
 const TopBarButtonWrapper = styled.button`
-  background-color: red;
+  background-color: white;
   display: flex;
   align-items: center;
   padding: 5px;
+  border: 0px;
 `
 
 const TopBarIcon = styled.img`
-  width: 15px;
-  background-color: yellow;
+  width: 20px;
+  background-color: white;
   margin-right: 5px;
+  margin-bottom: 5px;
 `
 const Plus = styled.img`
   width: 15px;
@@ -35,32 +39,34 @@ const Plus = styled.img`
 `
 
 const UserWrapper = styled.div`
-  background-color: gray;
+  background-color: transparent;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 25px 10px;
+  border-bottom: 1px solid ${Colors.grey};
 `
 const AvatarWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: pink;
+  background-color: transparent;
 `
 
 const Avatar = styled.div`
   box-sizing: border-box;
   min-width: 60px;
   height: 60px;
-  background-color: pink;
+  background-color: red;
+  border-radius: 50%;
 `
 
 const DetailsWrapper = styled.div`
   display: flex;
   width: 90%;
   justify-content: space-between;
-  background-color: green;
+  background-color: transparent;
 `
 const LeftInputs = styled.div`
   display: flex;
@@ -68,7 +74,15 @@ const LeftInputs = styled.div`
 
 `
 
-const NameInput = styled.input`
+const TopProfileInput = styled.input<{isEdit: boolean}>`
+  border: 0px;
+  margin-bottom: 5px;
+  padding: 5px;
+  
+  ${props => props.isEdit && css `
+    border: 1px solid ${Colors.grey};
+    /* background-color: red; */
+  `}
 
 `
 
@@ -83,6 +97,9 @@ const TypeInput = styled.input`
 const RightInputs = styled.div`
   display: flex;
   flex-direction: column;
+  background-color: transparent;
+  justify-content: space-between;
+  align-items: flex-end;
 `
 
 const EmailInput = styled.input`
@@ -94,72 +111,141 @@ const CustomInput = styled.input`
 `
 
 const UserDetailsWrapper = styled.div`
-  background-color: green;
+  background-color: transparent;
   display: flex;
   flex-direction: column;
   padding: 25px 10px;
+  border-bottom: 1px solid ${Colors.grey};
+  position: relative;
 `
 
+const DetailsIconEdit = styled.img`
+  width: 25px;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+`
+
+
+
 const TitleDetails = styled.span`
+  /* background-color: red; */
+
 
 `
 const InputWrapperDetails = styled.div`
-  margin-bottom: 10px;
+  margin: 10px;
 `
 
-const InputDetails = styled.input`
+const InputDetails = styled.input<{isDetailsEdit: boolean}>`
   margin-right: 10px;
+  background-color: #eceef7;
+  border: 0px;
+  padding: 5px;
+  color: #bbc6f7;
+
+  ${props => props.isDetailsEdit && css `
+    border: 1px solid ${Colors.grey};
+    /* background-color: red; */
+  `}
+
 `
 
 const PanelInformationsWrapper = styled.div`
-  background-color: yellow;
+  background-color: transparent;
   display: flex;
   flex-direction: column;
   padding: 25px 10px;
+  border-bottom: 1px solid ${Colors.grey};
 `
 
 const Title = styled.span`
-
+  padding: 10px;
+  /* background-color: pink; */
+  margin-bottom: 10px;
+  font-weight: bold;
+  font-size: ${fontSize[18]}
 `
 
 const ProfileTitleWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: red;
+  /* background-color: red; */
   margin-bottom: 10px;
+
 `
 
 const ProfileTitle = styled.span`
   max-width: 250px;
+  font-weight: bold;
+  margin-bottom: 5px;
+  color: ${Colors.grey}
 
 `
-const MoneyInput = styled.input`
+const MoneyInput = styled.input <{isDetailsEdit: boolean}>`
   max-width: 100px;
+  margin-bottom: 10px;
+  border: 0px;
+  padding: 5px;
 
+  ${props => props.isDetailsEdit && css `
+    border: 1px solid ${Colors.grey};
+    /* background-color: red; */
+  `}
 `
 
-const ProfileInput = styled.input`
+const ProfileInput = styled.input <{isDetailsEdit: boolean}>`
   max-width: 300px;
+  margin-bottom: 10px;
+  border: 0px;
+  padding: 5px;
 
+  ${props => props.isDetailsEdit && css `
+    border: 1px solid ${Colors.grey};
+    /* background-color: red; */
+  `}
+`
+
+const FileInput = styled.input <{isDetailsEdit: boolean}>`
+  max-width: 300px;
+  margin-bottom: 10px;
+  border: 0px;
+  padding: 5px;
+
+  ${props => props.isDetailsEdit && css `
+    border: 1px solid ${Colors.grey};
+    /* background-color: red; */
+  `}
 `
 
 const CorrespondantsWrapper = styled.div`
-  background-color: orange;
+  width: 70%;
+  background-color: #ebebeb;
   display: flex;
   margin-bottom: 5px;
+  padding: 5px;
+  display: flex;
+  align-items: center;
 `
 
 const CorrespondantsImg = styled.img`
   width: 40px;
   height: 40px;
+  border-radius: 50%;
   background-color: red;
   margin-right: 10px;
 `
 
-const CorrespondantsName = styled.input`
+const CorrespondantsName = styled.input  <{isDetailsEdit: boolean}>`
   max-width: 300px;
   margin-right: 10px;
-
+  height: 26px;
+  border: 0px;
+  background-color: #ebebeb;
+  ${props => props.isDetailsEdit && css `
+    border: 1px solid ${Colors.grey};
+    /* background-color: red; */
+  `}
 `
 
 const CorrespondantsButtonWrapper = styled.button`
@@ -167,21 +253,25 @@ const CorrespondantsButtonWrapper = styled.button`
   margin-right: 10px;
   display: flex;
   align-items: center;
+  border: 0px;
 `
 
 const ProposalsWrapper = styled.div`
-  background-color: green;
+  /* background-color: green; */
+  padding: 20px 10px;
+  border-bottom: 1px solid ${Colors.grey};
 `
 // td
 
 const TableHeader = styled.td`
-  background-color: pink;
+  /* background-color: pink; */
   width: 100px;
   padding: 10px;
+  font-weight: bold;
 `
 
 const TableCell = styled.td`
-  background-color: pink;
+  /* background-color: pink; */
   width: 100px;
   padding: 5px;
 `
@@ -189,14 +279,61 @@ const TableCell = styled.td`
 //thead
 
 const Thead = styled.thead`
+  border-bottom: 1px solid ${Colors.grey};
+`
+const CustomTable = styled.table`
+  margin-top: 10px;
+`
+
+const InternalReviewsWrapper = styled.div`
+  /* background-color: green; */
+  padding: 20px 10px;
+  border-bottom: 1px solid ${Colors.grey};
+`
+
+const AmountWrapper = styled.div`
+  /* background-color: green; */
+  padding: 20px 10px;
+`
+
+const AmountThead = styled.thead`
   border-bottom: 1px black solid;
+`
+
+const SubTitle = styled.span`
+  font-size: ${fontSize[16]};
+  font-weight: bold;
+  color: ${Colors.grey}
+`
+
+const DetailsTitle = styled.span`
+  /* font-size: ${fontSize[16]}; */
+  /* font-weight: bold; */
+  color: ${Colors.grey}
 `
 
 
 
+
+
+
 const Profile: FC = () => {
+
+  const [userEdit, setUserEdit] = useState<boolean>(false);
+    
+    const userEditHandler = () => {
+      setUserEdit(!userEdit);
+    }
+
+  const [userDetailsEdit, setUserEditDetails] = useState<boolean>(false);
+    
+  const userDetailsEditHandler = () => {
+    setUserEditDetails(!userDetailsEdit);
+  }
+
   return (
     <Wrapper>
+        {console.log(userEdit)}
         <TopBar>
           <TopBarButtonWrapper>
             <TopBarIcon src="./media/entities2.png"/>
@@ -224,45 +361,56 @@ const Profile: FC = () => {
           </AvatarWrapper>
           <DetailsWrapper>
             <LeftInputs>
-              <NameInput value="Imie"/>
-              <CityInput value="New-York" />
-              <TypeInput value="Profile" />
+              <TopProfileInput isEdit={userEdit} disabled={userEdit ? false : true} defaultValue="Imie"/>
+              <TopProfileInput isEdit={userEdit} disabled={userEdit ? false : true} defaultValue="New-York" />
+              <TopProfileInput isEdit={userEdit} disabled={userEdit ? false : true} defaultValue="Profile" />
             </LeftInputs>
             <RightInputs>
-              <TopBarIcon src="./media/entities2.png"/>
-              <EmailInput value="fasdfsaf@gmail.com" />
-              <CustomInput value="03 3213 312312" />
+              <TopBarIcon onClick={userEditHandler} src="./media/settings.svg"/>
+              <TopProfileInput isEdit={userEdit} disabled={userEdit ? false : true} defaultValue="fasdfsaf@gmail.com" />
+              <TopProfileInput isEdit={userEdit} disabled={userEdit ? false : true} defaultValue="03 3213 312312" />
             </RightInputs>
           </DetailsWrapper>
         </UserWrapper>
+
         <UserDetailsWrapper>
+          <DetailsIconEdit onClick={userDetailsEditHandler}  src="./media/settings.svg"/>
           <TitleDetails>
-            Expertise
+            <SubTitle>
+              Expertise
+
+            </SubTitle>
             <InputWrapperDetails>
-              <InputDetails value="Mergers and acquistions" />
+              <InputDetails isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true} defaultValue="Mergers and acquistions" />
             </InputWrapperDetails>
           </TitleDetails>
 
           <TitleDetails>
-            Specialties
+            <SubTitle>
+              Specialties
+            </SubTitle>
             <InputWrapperDetails>
-              <InputDetails value="Cross border operation" />
-              <InputDetails value="Transaction over 500" />
+              <InputDetails isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true} value="Cross border operation" />
+              <InputDetails isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true} value="Transaction over 500" />
             </InputWrapperDetails>
           </TitleDetails>
 
           <TitleDetails>
-            Admission to practice law
+            <SubTitle>
+              Admission to practice law
+            </SubTitle>
             <InputWrapperDetails>
-              <InputDetails value="Paris bar association" />
-              <InputDetails value="Tunisian bar assocation" />
+              <InputDetails isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true} value="Paris bar association" />
+              <InputDetails isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true} value="Tunisian bar assocation" />
             </InputWrapperDetails>
           </TitleDetails>
 
           <TitleDetails>
-            Counties
+            <SubTitle>
+              Counties
+            </SubTitle>
             <InputWrapperDetails>
-              <InputDetails value="Tunisia" />
+              <InputDetails isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true} value="Tunisia" />
             </InputWrapperDetails>
           </TitleDetails>
         </UserDetailsWrapper>
@@ -276,27 +424,22 @@ const Profile: FC = () => {
             <ProfileTitle>
               Hourly fee
             </ProfileTitle>
-            <MoneyInput value="610zl "/>
-          </ProfileTitleWrapper>
+            <MoneyInput isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true} defaultValue="610zl " />
 
-          <ProfileTitleWrapper>
             <ProfileTitle>
               Terms and conditions
             </ProfileTitle>
-            <ProfileInput value="Monthly 10k retainer - see with Jeanny Smith "/>
-          </ProfileTitleWrapper>
+            <ProfileInput isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true} defaultValue="Monthly 10k retainer - see with Jeanny Smith "/>
 
-          <ProfileTitleWrapper>
-            <ProfileInput value="jakis plik"/>
-          </ProfileTitleWrapper>
+            <FileInput isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true}  type="file" />
 
-          
-          <ProfileTitleWrapper>
             <ProfileTitle>
               Services and projects
             </ProfileTitle>
-            <ProfileInput value="Corporate MA and international acquisitions"/>
+            <ProfileInput isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true} defaultValue="Corporate MA and international acquisitions"/>
+
           </ProfileTitleWrapper>
+
 
           <Title>
             Internal correspondants
@@ -305,7 +448,7 @@ const Profile: FC = () => {
           <ProfileTitleWrapper>
             <CorrespondantsWrapper>
               <CorrespondantsImg />
-              <CorrespondantsName value="Firstname Lastname" />
+              <CorrespondantsName  isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true}  defaultValue="Firstname Lastname" />
 
               <CorrespondantsButtonWrapper>
                 <TopBarIcon src="./media/entities2.png"/>
@@ -320,7 +463,7 @@ const Profile: FC = () => {
 
             <CorrespondantsWrapper>
               <CorrespondantsImg />
-              <CorrespondantsName value="Firstname Lastname" />
+              <CorrespondantsName  isDetailsEdit={userDetailsEdit} disabled={userDetailsEdit ? false : true}  defaultValue="Firstname Lastname" />
 
               <CorrespondantsButtonWrapper>
                 <TopBarIcon src="./media/entities2.png"/>
@@ -341,7 +484,7 @@ const Profile: FC = () => {
             Proposals
           </Title>
 
-          <table>
+          <CustomTable>
           <Thead>
             <tr>
               <TableHeader>Name</TableHeader>
@@ -370,11 +513,79 @@ const Profile: FC = () => {
               <TableCell>Racine</TableCell>
             </tr>
           </tbody>
-          </table>
+          </CustomTable>
 
         </ProposalsWrapper>
 
+        <InternalReviewsWrapper>
 
+          <Title>
+            Internal reviews
+          </Title>
+
+         
+          <CustomTable>
+          <Thead>
+            <tr>
+              <TableHeader>Name</TableHeader>
+              <TableHeader>Entity</TableHeader>
+              <TableHeader>Location</TableHeader>
+              <TableHeader>Experiense</TableHeader>
+              <TableHeader>Date</TableHeader>
+            </tr>
+          </Thead>
+          <tbody>
+            <tr>
+              <TableCell>Operation</TableCell>
+              <TableCell>Renoult</TableCell>
+              <TableCell>France</TableCell>
+              <TableCell>#Tax</TableCell>
+              <TableCell>10/10/2010</TableCell>
+            </tr>
+            <tr>
+              <TableCell>Operation</TableCell>
+              <TableCell>Renoult</TableCell>
+              <TableCell>France</TableCell>
+              <TableCell>#Tax</TableCell>
+              <TableCell>10/10/2010</TableCell>
+            </tr>
+          </tbody>
+          </CustomTable>
+          <Link to="/NotFound">See more reviews</Link>
+        </InternalReviewsWrapper>
+
+        <AmountWrapper>
+
+          <Title>
+            Amount of fees
+          </Title>
+
+         
+          <CustomTable>
+          <thead>
+            <tr>
+              <TableHeader>Year</TableHeader>
+              <TableHeader>Cost center</TableHeader>
+              <TableHeader>Total amount</TableHeader>
+              <TableHeader>Law firm</TableHeader>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <TableCell>2019</TableCell>
+              <TableCell>CS 153</TableCell>
+              <TableCell>3500</TableCell>
+              <TableCell>Linklaters</TableCell>
+            </tr>
+            <tr>
+              <TableCell>2018</TableCell>
+              <TableCell>CS 153</TableCell>
+              <TableCell>3500</TableCell>
+              <TableCell>Linklaters</TableCell>
+            </tr>
+          </tbody>
+          </CustomTable>
+        </AmountWrapper>
 
     </Wrapper>
   );
